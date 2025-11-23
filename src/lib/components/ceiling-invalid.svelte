eb64376b5fe8d71113e6f9a1dcad5c041e63693f
warren.baugh <screenlantern@gmail.com>
2025-11-23 07:42:42 +0000

    tweak

--- src/lib/components/ceiling-invalid.svelte
+++ src/lib/components/ceiling-invalid.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Rect } from 'svelte-konva';
 
-	let { coords, width, height, fill = 'rgba(255, 0, 0, 0.5)' } = $props();
+	let { coords, width, height, fill = 'rgba(255, 0, 0, 0.2)' } = $props();
 	const [x, y] = coords;
 </script>
 
